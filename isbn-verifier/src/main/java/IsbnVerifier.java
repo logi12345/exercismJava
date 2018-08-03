b143f1c6c2b9d6ff82dbc8d1fd273cf8efc5689c
kimanhvu2018 <kim-anhv@hotmail.com>
2018-08-03 15:15:53 +0100

    update isbnverifier

--- isbn-verifier/src/main/java/IsbnVerifier.java
+++ isbn-verifier/src/main/java/IsbnVerifier.java
@@ -1,5 +1,5 @@
 class IsbnVerifier {
-    boolean verified;
+    boolean verified = true;
 
     boolean isValid(String stringToVerify) {
         stringToVerify = stringToVerify.replaceAll("-", ""); // removes all instances of '-' if there are any
@@ -11,30 +11,28 @@ class IsbnVerifier {
             verified = false;
         }
 
-        else if (x[9] < 0 || x[9] > 9 || x[9] == 'X') {
-            verified = false;
-        }
-
-        else {
+        else if (verified == true) {
             for (int i = 0; (i < 10) && verified == true; i++) {
                 if (x[i] < 0 || x[i] > 9) {
                     verified = false;
                 }
 
-                else {
-                    for(i = 0; i < 10; i++){
-                        y[i] = x[i] - 0;
-                    }
+                else if(x[i] == 'X') {
+                    y[i] = 10;
+                }
 
-                    if(x[i] == 'X') {
-                                y[i] = 10;
-                    }
+                else {
+
+                    y[i] = x[i] - 0;
+
                 }
             }
         }
 
-        if (((y[0] * 10 + y[1] * 9 + y[2] * 8 + y[3] * 7 + y[4] * 6 + y[5] * 5 + y[6] * 4 + y[7] * 3 + y[8] * 2 + y[9]) % 11) != 0) {
-            verified = false;
+        else if(verified == true){
+            if (((y[0] * 10 + y[1] * 9 + y[2] * 8 + y[3] * 7 + y[4] * 6 + y[5] * 5 + y[6] * 4 + y[7] * 3 + y[8] * 2 + y[9]) % 11) != 0) {
+                verified = false;
+            }
         }
         return verified;
     }
