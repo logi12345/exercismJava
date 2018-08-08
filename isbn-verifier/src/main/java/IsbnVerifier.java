b7c530b41a26a075086deda1bccadd6ba074f9d9
kimanhvu2018 <kim-anhv@hotmail.com>
2018-08-08 23:00:57 +0100

    added lines to find error

--- isbn-verifier/src/main/java/IsbnVerifier.java
+++ isbn-verifier/src/main/java/IsbnVerifier.java
@@ -1,14 +1,25 @@
 class IsbnVerifier {
-    boolean verified = true;
 
-    boolean isValid(String stringToVerify) {
+
+    public static void main(String[] args){
+        isValid("3-598-21508-8"); //all true
+        // isValid("3598215088");
+       // isValid("359821507X");
+      //  isValid("3-598-21507-X");
+    }
+
+    static boolean isValid(String stringToVerify) {
+        boolean verified = true;
         stringToVerify = stringToVerify.replaceAll("-", ""); // removes all instances of '-' if there are any
-
+        System.out.println(String.valueOf(verified));
         char[] x = stringToVerify.toCharArray();
         int [] y = new int[10];
 
+        System.out.println(stringToVerify);
+
         if (stringToVerify.length() != 10) {    // makes sure there are 10 numbers in the string
             verified = false;
+            System.out.println(String.valueOf(verified));
         }
 
         else if (verified == true) {
@@ -16,6 +27,7 @@ class IsbnVerifier {
                 int a = Character.getNumericValue(x[i]);
                 if (a < 0 || a > 9 || a != 33) {
                     verified = false;
+                    System.out.println(String.valueOf(verified));
                 }
 
                 else if(a == 33) { //if x[i] == X
@@ -31,8 +43,10 @@ class IsbnVerifier {
         else if(verified == true){
             if (((y[0] * 10 + y[1] * 9 + y[2] * 8 + y[3] * 7 + y[4] * 6 + y[5] * 5 + y[6] * 4 + y[7] * 3 + y[8] * 2 + y[9]) % 11) != 0) {
                 verified = false;
+                System.out.println(String.valueOf(verified));
             }
         }
+        System.out.println(String.valueOf(verified));
         return verified;
     }
 
