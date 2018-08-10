462f3e71fe3454ca8cadd7f0cdd765a81dc84a39
Logi Gunaratnam <34453012+logi12345@users.noreply.github.com>
2018-08-10 17:18:11 +0100

    Add files via upload

--- isbn-verifier/src/main/java/IsbnVerifier.java
+++ isbn-verifier/src/main/java/IsbnVerifier.java
@@ -1,54 +1,74 @@
+
+
 class IsbnVerifier {
 
 
     public static void main(String[] args){
-        isValid("3-598-21508-8"); //all true
-        // isValid("3598215088");
-       // isValid("359821507X");
-      //  isValid("3-598-21507-X");
+        System.out.println(isValid("3-598-21508-8")); //all true
+        isValid("3598215088");
+        isValid("359821507X");
+        isValid("3-598-21507-X");
     }
 
-    static boolean isValid(String stringToVerify) {
-        boolean verified = true;
-        stringToVerify = stringToVerify.replaceAll("-", ""); // removes all instances of '-' if there are any
-        System.out.println(String.valueOf(verified));
-        char[] x = stringToVerify.toCharArray();
-        int [] y = new int[10];
+    /**
+     * Checks if ISBN entered is valid.
+     * @param x
+     * @return boolean
+     */
+    static boolean isValid(String x){
 
-        System.out.println(stringToVerify);
+        //split the original string inorder to remove the dashes.
+        String[] temp = x.split("-");
 
-        if (stringToVerify.length() != 10) {    // makes sure there are 10 numbers in the string
-            verified = false;
-            System.out.println(String.valueOf(verified));
-        }
+        //join the string back together.
+        x = String.join("",temp);
 
-        else if (verified == true) {
-            for (int i = 0; (i < 10) && verified == true; i++) {
-                int a = Character.getNumericValue(x[i]);
-                if (a < 0 || a > 9 || a != 33) {
-                    verified = false;
-                    System.out.println(String.valueOf(verified));
+        //ISBN length is always 10 so create array of size 10.
+        String[] isbn = new String[10];
+
+        //Loop through the array and take each character convert to String and check if it is valid
+        for (int i=0; i<x.length(); i++ ){
+            char c = x.charAt(i);
+            String b= Character.toString(c);
+            if ((i!=9) && !b.equalsIgnoreCase("x") ){
+                isbn[i] = b;
+            }
+            //If there is an x at the end of the series add to 10 the array
+            else if ((i==9)) {
+                if(b.equalsIgnoreCase("x")){
+                    isbn[i] = "10";
                 }
-
-                else if(a == 33) { //if x[i] == X
-                    y[i] = 10;
-                }
-
-                else {
-                    y[i] = a;
+                else{
+                    isbn[i]=b;
                 }
             }
-        }
+            else
+            {
 
-        else if(verified == true){
-            if (((y[0] * 10 + y[1] * 9 + y[2] * 8 + y[3] * 7 + y[4] * 6 + y[5] * 5 + y[6] * 4 + y[7] * 3 + y[8] * 2 + y[9]) % 11) != 0) {
-                verified = false;
-                System.out.println(String.valueOf(verified));
+                return false;
             }
         }
-        System.out.println(String.valueOf(verified));
-        return verified;
+       return calculateISBN(isbn);
     }
+
+    /**
+     * Check if ISBN is Valid.
+      * @param isbn
+     * @return boolean
+     */
+    static boolean calculateISBN(String[] isbn)
+    {
+        //convert String Arrray into string of numbers.
+        int[] intISBN= new int[isbn.length];
+        int i=0;
+        for(String str:isbn){
+            intISBN[i]=Integer.parseInt(str);
+            i++;
+        }
+
+       return (intISBN[0] * 10 + intISBN[1] * 9 + intISBN[2] * 8 + intISBN[3] * 7 + intISBN[4] * 6 + intISBN[5]  * 5 + intISBN[6]  * 4 + intISBN[7]  * 3 + intISBN[8]  * 2 + intISBN[9] * 1)%11 == 0;
+    }
+
 
 }
 
